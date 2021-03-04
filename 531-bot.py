76654393cf8d4a08103baa6b4691047395148ded
Alexander Bridgeman <abridgeman@CWTitle.LAN>
2021-03-03 16:13:53 -0800

    overhaul

--- 531-bot.py
+++ 531-bot.py
@@ -2,13 +2,40 @@ import discord
 import random
 client = discord.Client()
 
-# 1RM variables
-press_1RM = 115
-deadlift_1RM = 310
-bench_1RM = 200
-squat_1RM = 245
+TOKEN = 'NzYzOTY0MDc2OTA4ODcxNjkx.X3_W_Q.89GLeoxRHI5fd2OEINFS2Tu57So'
+channelID = 744728945873780739
 
+#function that rounds to nearest 5
+def myround(x, base=5):
+    return base * round(x/base)
 
+class Workout:
+    def __init__(self, exercise, maxweight):
+        self.exercise = exercise
+        self.maxweight = maxweight
+
+    def todaysWorkout(self):
+        setsList = []
+
+        setsList.append(myround(self.maxweight * self.set1))
+        setsList.append(myround(self.maxweight * self.set2))
+        setsList.append(myround(self.maxweight * self.set3))
+
+        return setsList
+
+w1 = {0.65: ' x 5', 0.75: ' x 5', 0.85: ' x 5+'}
+w2 = {0.7: ' x 3', 0.8: ' x 3', 0.9: ' x 3+'}
+w3 = {0.75: ' x 5', 0.85: ' x 3', 0.95: ' x 1+'}
+w4 = {0.4: ' x 5', 0.5: ' x 5', 0.6: ' x 5'}
+
+dictDict = {'w1': w1, 'w2':w2, 'w3':w3, 'w4':w4}
+
+# these numbers are the 1RM, change these each month
+press = Workout('press', 100)
+squat = Workout('squat', 250)
+deadlift = Workout('deadlift', 315)
+bench = Workout('bench', 200)
+exerciseDict = {'press': press, 'squat': squat, 'deadlift': deadlift, 'bench':bench}
 
 press_accessories_upperchest = ['incline bench','60 degree handstand-pushup','low-to-high cable fly','jammer press machine','pike pushup']
 press_accessories_triceps = ['dips','close grip bench','bodyweight skullcrusher','close grip pushup','overhead extension']
@@ -22,190 +49,61 @@ bench_accessories_reardelt = ['rear dumbbell flye','reverse pec deck','face pull
 squat_accessories_quads = ['single-leg trx handle squat','trx band single-leg lunge','machine hack squat','barbell hack squat','box jump']
 squat_accessories_calves = deadlift_accessories_calves
 
-press_alternative = []
-deadlift_alternative = []
-bench_alternative = []
-squat_alternative = []
-
-#function that rounds to nearest 5!
-def myround(x, base=5):
-    return base * round(x/base)
-
-#functions for workouts!
-def weekOne(workout):
-    setsList = []
-    
-    setsList.append(myround(workout * .6))
-    setsList.append(myround(workout * .7))
-    setsList.append(myround(workout * .8))
-    return setsList
-
-def weekTwo(workout):
-    setsList = []
-    
-    setsList.append(myround(workout * .65))
-    setsList.append(myround(workout * .75))
-    setsList.append(myround(workout * .85))
-    return setsList
-
-def weekThree(workout):
-    setsList = []
-    
-    setsList.append(myround(workout * .7))
-    setsList.append(myround(workout * .8))
-    setsList.append(myround(workout * .9))
-    return setsList
-
-def weekFour(workout):
-    setsList = []
-    
-    setsList.append(myround(workout * .4))
-    setsList.append(myround(workout * .5))
-    setsList.append(myround(workout * .6))
-    return setsList
-
-fiveThreeOne = ['zoop',5,3,1]
-
 #bot stuff!
 @client.event
 async def on_ready():
-    general_channel = client.get_channel(YOUR CHANNEL ID HERE)
+    general_channel = client.get_channel(channelID)
     print('Connected Successfully')
 
 @client.event
 async def on_message(message):
-    general_channel = client.get_channel(YOUR CHANNEL ID HERE)
+    general_channel = client.get_channel(channelID)
 
-    if message.author == client.user:
-        return
+    if message.content.startswith('$w'):
+        workoutMessage = message.content
+        messageWeek = workoutMessage[1:3]
+        workoutMessage = workoutMessage.split()
+        messageExercise = workoutMessage[1]
+        
+        floatsList = list(dictDict[messageWeek].keys())
+        repsList = list(dictDict[messageWeek].values())
 
-    if message.content.startswith('hi'):
-        await general_channel.send('hi')
+        exerciseDict[messageExercise].set1 = floatsList[0]
+        exerciseDict[messageExercise].set2 = floatsList[1]
+        exerciseDict[messageExercise].set3 = floatsList[2]
+        
+        setsList = (exerciseDict[messageExercise]).todaysWorkout()
+        print(setsList)
+        print(type(setsList))
 
-    if message.content.startswith('$week '):
-        workout_message = message.content.split('$week ',1)[1] 
-
-        if workout_message.startswith('1'):
-            workout_message = message.content.split('1 ',1)[1]   
-            if workout_message == "press":      
-                for i, weight in enumerate(weekOne(press_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
+        async def print_sets():
+            if messageExercise == 'press':
+                for i in range(0,3):
+                    await general_channel.send(f'Set #{i + 1}: {setsList[i]} {repsList[i]}')
                 await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5  x 10')
                 await general_channel.send(str(random.choice(press_accessories_triceps)) + ' 5 x 10')
 
-            if workout_message == "deadlift":      
-                for i, weight in enumerate(weekOne(deadlift_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
-                await general_channel.send(str(random.choice(deadlift_accessories_calves)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(deadlift_accessories_hamstrings)) + ' 5 x 10')
-
-            if workout_message == "bench":      
-                for i, weight in enumerate(weekOne(bench_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
-                await general_channel.send(str(random.choice(bench_accessories_notreardelt)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(bench_accessories_reardelt)) + ' 5 x 10')
-
-            if workout_message == "squat":      
-                for i, weight in enumerate(weekOne(squat_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
-                await general_channel.send(str(random.choice(squat_accessories_quads)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(squat_accessories_calves)) + ' 5 x 10')
-
-
-        if workout_message.startswith('2'):      
-            workout_message = message.content.split('2 ',1)[1]    
-            if workout_message == "press":      
-                for i, weight in enumerate(weekTwo(press_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 3')
-
-                await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5 x 10')
+            elif messageExercise == 'deadlift':
+                for i in range(0,3):
+                    await general_channel.send(f'Set #{i + 1}: {setsList[i]} {repsList[i]}')
+                await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5  x 10')
                 await general_channel.send(str(random.choice(press_accessories_triceps)) + ' 5 x 10')
 
-            if workout_message == "deadlift":      
-                for i, weight in enumerate(weekTwo(deadlift_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 3')
-
-                await general_channel.send(str(random.choice(deadlift_accessories_calves)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(deadlift_accessories_hamstrings)) + ' 5 x 10')
-
-            if workout_message == "bench":      
-                for i, weight in enumerate(weekTwo(bench_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 3')
-
-                await general_channel.send(str(random.choice(bench_accessories_notreardelt)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(bench_accessories_reardelt)) + ' 5 x 10')
-
-            if workout_message == "squat":      
-                for i, weight in enumerate(weekTwo(squat_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 3')
-
-                await general_channel.send(str(random.choice(squat_accessories_quads)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(squat_accessories_calves)) + ' 5 x 10')
-
-
-        if workout_message.startswith('3'):   
-            workout_message = message.content.split('3 ',1)[1]   
-            if workout_message == "press":           
-                for i, weight in enumerate(weekThree(press_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x ' + str(fiveThreeOne[i + 1]))
-
-                await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5 x 10')
+            elif messageExercise == 'bench':
+                for i in range(0,3):
+                    await general_channel.send(f'Set #{i + 1}: {setsList[i]} {repsList[i]}')
+                await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5  x 10')
                 await general_channel.send(str(random.choice(press_accessories_triceps)) + ' 5 x 10')
 
-            if workout_message == "deadlift":      
-                for i, weight in enumerate(weekThree(deadlift_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x ' + str(fiveThreeOne[i + 1]))
-
-                await general_channel.send(str(random.choice(deadlift_accessories_calves)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(deadlift_accessories_hamstrings)) + ' 5 x 10')
-
-            if workout_message == "bench":      
-                for i, weight in enumerate(weekThree(bench_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x ' + str(fiveThreeOne[i + 1]))
-
-                await general_channel.send(str(random.choice(bench_accessories_notreardelt)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(bench_accessories_reardelt)) + ' 5 x 10')
-
-            if workout_message == "squat":      
-                for i, weight in enumerate(weekThree(squat_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x ' + str(fiveThreeOne[i + 1]))
-
-                await general_channel.send(str(random.choice(squat_accessories_quads)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(squat_accessories_calves)) + ' 5 x 10')
-
-
-        if workout_message.startswith('4'):     
-            workout_message = message.content.split('4 ',1)[1]    
-            if workout_message == "press":
-                for i, weight in enumerate(weekFour(press_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
-                await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5 x 10')
+            elif messageExercise == 'squat':
+                for i in range(0,3):
+                    await general_channel.send(f'Set #{i + 1}: {setsList[i]} {repsList[i]}')
+                await general_channel.send(str(random.choice(press_accessories_upperchest)) + ' 5  x 10')
                 await general_channel.send(str(random.choice(press_accessories_triceps)) + ' 5 x 10')
-                
-            if workout_message == "deadlift":      
-                for i, weight in enumerate(weekFour(deadlift_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
+            
+            else:
+                print('zoop')
 
-                await general_channel.send(str(random.choice(deadlift_accessories_calves)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(deadlift_accessories_hamstrings)) + ' 5 x 10')
+        await print_sets()
 
-            if workout_message == "bench":      
-                for i, weight in enumerate(weekFour(bench_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
-                await general_channel.send(str(random.choice(bench_accessories_notreardelt)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(bench_accessories_reardelt)) + ' 5 x 10')
-
-            if workout_message == "squat":    
-                for i, weight in enumerate(weekFour(squat_1RM)):
-                    await general_channel.send(f"Set #{i + 1}: " + str(weight) + ' x 5')
-
-                await general_channel.send(str(random.choice(squat_accessories_quads)) + ' 5 x 10')
-                await general_channel.send(str(random.choice(squat_accessories_calves)) + ' 5 x 10')
-
-client.run('your token here')
+client.run(TOKEN)
